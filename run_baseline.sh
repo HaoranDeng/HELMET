2990d775c3c16504f56724e29931243f1c7e5072
HaoranDeng <deng.hr@163.com>
2025-07-29 18:11:15 +0000

    update

--- run_baseline.sh
+++ run_baseline.sh
@@ -7,5 +7,5 @@ export model_name="OpenPAI-LongContext-17BA3B-32k-ds0714-rb1e5-remix"
 CUDA_VISIBLE_DEVICES=0,1 bash run_hfhub.sh meta-llama/Meta-Llama-3-8B &
 CUDA_VISIBLE_DEVICES=2,3 bash run_hfhub.sh meta-llama/Meta-Llama-3.1-8B &
 CUDA_VISIBLE_DEVICES=4,5 bash run_hfhub.sh Qwen/Qwen-4B &
-CUDA_VISIBLE_DEVICES=6,7 bash run_sigma.sh Qwen/Qwen-8B &
+CUDA_VISIBLE_DEVICES=6,7 bash run_hfhub.sh Qwen/Qwen-8B &
 wait
