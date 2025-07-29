eee5a2d708525ea7de0c38687400faf41ef3e531
HaoranDeng <deng.hr@163.com>
2025-07-29 16:34:21 +0000

    update

---
+++ run_rb5e5_remix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+bash scripts/download_data.sh
+export model_name="OpenPAI-LongContext-17BA3B-32k-ds0714-rb5e5-remix"
+
+CUDA_VISIBLE_DEVICES=0,1 bash run_sigma.sh 200 &
+CUDA_VISIBLE_DEVICES=2,3 bash run_sigma.sh 400 &
+CUDA_VISIBLE_DEVICES=4,5 bash run_sigma.sh 600 &
+CUDA_VISIBLE_DEVICES=6,7 bash run_sigma.sh 800 &
+wait
+
+CUDA_VISIBLE_DEVICES=0,1 bash run_sigma.sh 1000 &
+CUDA_VISIBLE_DEVICES=2,3 bash run_sigma.sh 1200 &
+CUDA_VISIBLE_DEVICES=4,5 bash run_sigma.sh 1400 &
+CUDA_VISIBLE_DEVICES=6,7 bash run_sigma.sh 1600 &
+wait
+
+CUDA_VISIBLE_DEVICES=0,1 bash run_sigma.sh 1800 &
+CUDA_VISIBLE_DEVICES=2,3 bash run_sigma.sh 2000 &
+wait
