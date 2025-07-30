ebcc5ded71c86272356fa8576390c6421e86e067
HaoranDeng <deng.hr@163.com>
2025-07-30 06:28:16 +0000

    update

--- run_baseline.sh
+++ run_baseline.sh
@@ -7,8 +7,8 @@ export model_name="OpenPAI-LongContext-17BA3B-32k-ds0714-rb1e5-remix"
 mkdir -p data
 cp -r /mnt/blob-pretraining-hptrainingwestcentralus/haoran_result/helmet_data/* data/.
 
-CUDA_VISIBLE_DEVICES=0 bash run_hfhub.sh meta-llama/Meta-Llama-3-8B &
-CUDA_VISIBLE_DEVICES=1 bash run_hfhub.sh meta-llama/Meta-Llama-3.1-8B &
+# CUDA_VISIBLE_DEVICES=0 bash run_hfhub.sh meta-llama/Meta-Llama-3-8B &
+# CUDA_VISIBLE_DEVICES=1 bash run_hfhub.sh meta-llama/Meta-Llama-3.1-8B &
 CUDA_VISIBLE_DEVICES=2 bash run_hfhub.sh Qwen/Qwen3-4B &
 CUDA_VISIBLE_DEVICES=3 bash run_hfhub.sh Qwen/Qwen3-8B &
 wait
